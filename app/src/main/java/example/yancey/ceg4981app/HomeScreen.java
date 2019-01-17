6c853523639daf8704180afc89007e3fcf6bb860
madison_yancey <yancey.12@wright.edu>
2019-01-17 08:56:23 -0500

    Initial commit

---
+++ app/src/main/java/example/yancey/ceg4981app/HomeScreen.java
@@ -0,0 +1,13 @@
+package example.yancey.ceg4981app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
