6ebc06fc2f77ef9ff1b86d07646270bd3c32893a
madison_yancey <yancey.12@wright.edu>
2019-02-19 00:20:25 -0500

    Cook, Recipe, Schedule, & Manual Cook screens

---
+++ app/src/main/java/example/yancey/ceg4981app/ManualCookScreen.java
@@ -0,0 +1,13 @@
+package example.yancey.ceg4981app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManualCookScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manual_cook_screen);
+    }
+}
---
+++ app/src/main/java/example/yancey/ceg4981app/ScheduleScreen.java
@@ -0,0 +1,13 @@
+package example.yancey.ceg4981app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_screen);
+    }
+}
