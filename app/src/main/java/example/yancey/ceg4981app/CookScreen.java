b9ff7451dd0cac99ab7083fbd852a6b5398d07b1
madison_yancey <yancey.12@wright.edu>
2019-02-24 15:32:11 -0500

    More screen navigation

--- app/src/main/java/example/yancey/ceg4981app/CookScreen.java
+++ app/src/main/java/example/yancey/ceg4981app/CookScreen.java
@@ -1,7 +1,10 @@
 package example.yancey.ceg4981app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CookScreen extends AppCompatActivity {
 
@@ -9,5 +12,42 @@ public class CookScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cook_screen);
+
+        Button btnRecipes = findViewById(R.id.btnRecipes);
+        Button btnSchedule = findViewById(R.id.btnSchedule);
+        Button btnManualCook = findViewById(R.id.btnManual);
+
+        final Intent intentRecipes = new Intent(this, RecipesScreen.class);
+        intentRecipes.putExtra("Thing to do", "Go to recipes screen");
+
+        final Intent intentSchedules = new Intent(this, ScheduleScreen.class);
+        intentSchedules.putExtra("Thing to do", "Go to schedules screen");
+
+        final Intent intentManual = new Intent(this, ManualCookScreen.class);
+        intentManual.putExtra("Thing to do", "Go to manual cook screen");
+
+        btnRecipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(intentRecipes);
+            }
+        });
+
+        btnSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(intentSchedules);
+            }
+        });
+
+        btnManualCook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(intentManual);
+            }
+        });
     }
 }
--- app/src/main/java/example/yancey/ceg4981app/HomeScreen.java
+++ app/src/main/java/example/yancey/ceg4981app/HomeScreen.java
@@ -1,7 +1,11 @@
 package example.yancey.ceg4981app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        Button btnCook = findViewById(R.id.btnCook);
+        final Intent intent = new Intent(this, CookScreen.class);
+        intent.putExtra("Thing to do", "Go to cook screen");
+
+        btnCook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(intent);
+            }
+        });
+
     }
 }
