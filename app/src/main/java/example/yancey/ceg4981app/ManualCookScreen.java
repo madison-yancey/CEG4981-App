f23f88593d7f6028002c1885239a702d6c9d3af2
madison_yancey <yancey.12@wright.edu>
2019-04-15 23:59:58 -0400

    Outline manual cook screen & color changes

--- app/src/main/java/example/yancey/ceg4981app/ManualCookScreen.java
+++ app/src/main/java/example/yancey/ceg4981app/ManualCookScreen.java
@@ -2,6 +2,9 @@ package example.yancey.ceg4981app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ManualCookScreen extends AppCompatActivity {
 
@@ -9,5 +12,49 @@ public class ManualCookScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manual_cook_screen);
+
+        Button btnLow = findViewById(R.id.btnLow);
+        Button btnHigh = findViewById(R.id.btnHigh);
+        Button btnWarm = findViewById(R.id.btnWarm);
+        Button btnCookManual = findViewById(R.id.btnCookManual);
+        final TextView tvSetting2 = findViewById(R.id.tvSetting2);
+
+        //TO DO: only change setting when someone hits cook
+        // NO NO NO: save previous setting in case back button is pressed
+        //Button btnBack = findViewByID(R.id.btnBack);
+        //on click--get previous setting and SET previous setting
+
+        btnLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                tvSetting2.setText("Low");
+            }
+        });
+
+        btnHigh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                tvSetting2.setText("High");
+            }
+        });
+
+        btnWarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                tvSetting2.setText("Warm");
+            }
+        });
+
+        btnCookManual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String setting = (String) tvSetting2.getText();
+                //set setting to this (probably change to int)
+            }
+        });
     }
 }
